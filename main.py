94d57da63b0b31d8b96d5066173269ea72b160e9
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-08 17:12:14 +0530

    commit

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
-
-
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table
 from databases import Database
-
-
-
+import random
+from faker import Faker
 
 DATABASE_URL = "postgresql://postgres:Shantanu8983@@localhost:5432/UrlTask"
 
@@ -18,34 +14,111 @@ products = Table(
     "products",
     metadata,
     Column("id", Integer, primary_key=True),
-    Column("Product/Service", String),
-    Column("price", Integer),
-    Column("quantity", Integer),
-    Column("unit", String),
-    Column("weight", Integer),
-    Column("description", String),
+    Column("ArticleNo",Integer ),  # Changed to use underscore instead of /
+    Column("Product", String),
+    Column("inPrice", Integer),
+    Column("Price", Integer),
+    Column("Unit", String),
+    Column("inStock", Integer),
+    Column("Description", String),
 )
 
 
 
 app = FastAPI()
 
-
-
-
-
- # 
+# CORS middleware
 origins = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"], 
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-#
 
+# Function to initialize database connection pool
+async def startup():
+    await database.connect()
+
+# Function to close database connection pool
+async def shutdown():
+    await database.disconnect()
+
+# Event handlers for startup and shutdown
+app.add_event_handler("startup", startup)
+app.add_event_handler("shutdown", shutdown)
+
+# Function to insert product into the database
+async def insert_product(product_data):
+    query = products.insert().values(product_data)
+    return await database.execute(query)
+
+fake = Faker()
+
+
+
+
+
+
+async def get_products():
+    query = products.select()
+    return await database.fetch_all(query)
+
+# def generate_product_data():
+    return {
+        "Product/Service": fake.word(),
+        "Price": random.randint(1, 1000),
+        "Quantity": random.randint(1, 100),
+        "Unit": random.choice(["Piece", "Liter", "Kg"]),
+        "Weight": random.randint(1, 100),
+        "Description": fake.sentence()
+    }
+
+
+import random
+
+def generate_random_data(count):
+    data = []
+    for i in range(count):
+        random_article_no = random.randint(1, 10000000000)
+        random_product_name = f"Product {i + 1}"
+        random_in_price = random.randint(1, 100000)
+        random_price = random.randint(1, 2000000)
+        random_unit = random.choice(['Piece', 'Kg', 'Liter'])
+        random_in_stock = random.randint(1, 100)
+        random_description = f"Description for Product {i + 1}"
+
+        data.append(
+            {
+                "ArticleNo":random_article_no,
+                "Product": random_product_name,
+                "inPrice": random_in_price,
+                "Price": random_price,
+                "Unit":  random_unit,
+                "inStock": random_in_stock,
+                "Description": random_description 
+            }
+        )
+    return data
+
+# Example usage:
+random_data = generate_random_data(5)
+print(random_data)
+
+
+
+
+# Route to add product
+@app.post("/add-product")
+async def add_product(product_data: dict):
+    product_id = await insert_product(product_data)
+    return {"message": f"Product added with ID: {product_id}"}
+
+# Generate 20 product data
+async def add_products(product_data: dict):
+    product_id = await insert_product(product_data)
+    return {"message": f"Product added with ID: {product_id}"}
 
 
 
@@ -57,208 +130,31 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    print("hii")
+    # data = generate_random_data(20)
+    # for i in data:
+    #     out=await add_products(i) 
+    #     print(out,"out")
     return {"message": "Hello World"}
 
-
 @app.get("/terms")
-async def root():
-    return {"message": "VED Å klikke på Fakturere Nå så velger dere å laste ned ifølge den informasjon som dere har lagt inn og teksten på last ned siden og vilkårene her, og aksepterer samtidig vilkårene her."}
+async def get_terms():
+    return {"message": "Your terms message here"}
 
 @app.get("/us")
-async def root():
-    return {"message": "VED Å klikke på Fakturere Nå så velger dere å laste ned ifølge den informasjon som dere har lagt inn og teksten på last ned siden og vilkårene her, og aksepterer samtidig vilkårene her."}
-
-
+async def get_us():
+    return {"message": "Your US message here"}
 
 @app.get("/ok")
-async def root():
-    print("hii")
-    return{"message":"hii pro level"}
-
+async def get_ok():
+    return {"message": "hii pro level"}
 
 @app.get("/getProducts")
-async def root():
-    print("getting")
-    return {"message": [
-    [
-        658313245,
-        "Product 1",
-        74373,
-        965146,
-        "Piece",
-        50,
-        "Description for Product 1"
-    ],
-    [
-        4662520676,
-        "Product 2",
-        94422,
-        1488416,
-        "Piece",
-        31,
-        "Description for Product 2"
-    ],
-    [
-        5620616523,
-        "Product 3",
-        21004,
-        1102736,
-        "Liter",
-        55,
-        "Description for Product 3"
-    ],
-    [
-        5795135149,
-        "Product 4",
-        5355,
-        1057295,
-        "Kg",
-        25,
-        "Description for Product 4"
-    ],
-    [
-        2792474651,
-        "Product 5",
-        36471,
-        1866439,
-        "Liter",
-        67,
-        "Description for Product 5"
-    ],
-    [
-        3992986810,
-        "Product 6",
-        85011,
-        1940633,
-        "Liter",
-        69,
-        "Description for Product 6"
-    ],
-    [
-        1585499227,
-        "Product 7",
-        73032,
-        462132,
-        "Liter",
-        54,
-        "Description for Product 7"
-    ],
-    [
-        7164454206,
-        "Product 8",
-        31852,
-        1173817,
-        "Liter",
-        37,
-        "Description for Product 8"
-    ],
-    [
-        1842725101,
-        "Product 9",
-        80751,
-        147466,
-        "Piece",
-        22,
-        "Description for Product 9"
-    ],
-    [
-        3134255904,
-        "Product 10",
-        63057,
-        897078,
-        "Liter",
-        5,
-        "Description for Product 10"
-    ],
-    [
-        5961243443,
-        "Product 11",
-        83079,
-        355441,
-        "Piece",
-        14,
-        "Description for Product 11"
-    ],
-    [
-        3176823143,
-        "Product 12",
-        93498,
-        150544,
-        "Kg",
-        11,
-        "Description for Product 12"
-    ],
-    [
-        4068386366,
-        "Product 13",
-        50369,
-        1833492,
-        "Kg",
-        38,
-        "Description for Product 13"
-    ],
-    [
-        5529130347,
-        "Product 14",
-        29339,
-        1033242,
-        "Kg",
-        37,
-        "Description for Product 14"
-    ],
-    [
-        9551193865,
-        "Product 15",
-        38089,
-        1451108,
-        "Kg",
-        93,
-        "Description for Product 15"
-    ],
-    [
-        6488284121,
-        "Product 16",
-        11967,
-        1329809,
-        "Liter",
-        0,
-        "Description for Product 16"
-    ],
-    [
-        4127634226,
-        "Product 17",
-        79386,
-        1540097,
-        "Liter",
-        8,
-        "Description for Product 17"
-    ],
-    [
-        4336397227,
-        "Product 18",
-        65413,
-        336714,
-        "Kg",
-        20,
-        "Description for Product 18"
-    ],
-    [
-        1094843927,
-        "Product 19",
-        66161,
-        420710,
-        "Liter",
-        79,
-        "Description for Product 19"
-    ],
-    [
-        9379837477,
-        "Product 20",
-        97488,
-        969639,
-        "Piece",
-        2,
-        "Description for Product 20"
-    ]
-]}
+async def get_products_route():
+    products_data = await get_products()
+    converted_list = [
+            [product["ArticleNo"], product["Product"], product["inPrice"], product["Price"],
+            product["Unit"], product["inStock"], product["Description"]]
+            for product in products_data
+        ]
+    print(converted_list,"converted_list")
+    return {"message": converted_list}
